5181992b65b3aed7ad67d2b46c8bb7d7afa1fda2
Matthew Wells <mattwells9@shaw.ca>
2023-10-18 20:19:44 -0500

    first commit

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,7 +3,12 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+// functions that return nothing can just return () option is not needed
+fn call_me() -> () {
+    println!("hello!");
+}
 
 fn main() {
     call_me();
