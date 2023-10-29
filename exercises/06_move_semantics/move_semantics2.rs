f1f2ddedcc41102da6f4d744935c7727d3738cb7
Matthew Wells <mattwells9@shaw.ca>
2023-10-28 19:55:29 -0500

    updated move semantic excercises

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -5,13 +5,14 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
+    let vect = vec0.clone();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vect);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
