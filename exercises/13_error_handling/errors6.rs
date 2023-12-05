4a902f94d1466a6b8a479b0351edfbf60ec0276c
Matthew Wells <mattwells9@shaw.ca>
2023-12-04 22:33:22 -0600

    finished errors, need to review further

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -26,12 +26,13 @@ impl ParsePosNonzeroError {
     }
     // TODO: add another error conversion function here.
     // fn from_parseint...
+    fn from_parseint(x : ParseIntError) -> ParsePosNonzeroError { ParsePosNonzeroError::ParseInt(x) }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
